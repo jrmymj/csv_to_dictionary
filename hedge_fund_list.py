8b3a61782fbf1f966baf1725f624f456238417aa
jrmymj <vallesterosjeremy@gmail.com>
2017-03-31 23:39:20 -0700

    Update hedge_fund_list.py

--- hedge_fund_list.py
+++ hedge_fund_list.py
@@ -10,8 +10,6 @@ with open('Local Hedge Funds v2.csv') as csv_data:
     for row in rows[1:]:
         for col_header, data_column in zip(headings, row):
             hf_info.setdefault(col_header, []).append(data_column)
-
-
-
+            
 with open('local_hedge_funds_v2.txt', 'w') as f:
     print(hf_info, file=f)
